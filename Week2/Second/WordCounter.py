25721ea660e6f4a11a5f0673605a88c69a38ba29
ibrahimthaci <ibrahim_thaqi@outlook.com>
2021-03-28 16:04:19 +0200

    Add files via upload

---
+++ Week2/Second/WordCounter.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+from matplotlib import pyplot as plt
+
+
+# 2.Me lexu nje liber, text te gjate, me kodet e javes se kalume me
+# dictionaries me lexu fjalet qe perseriten disa her dhe mu paraqit grafikisht
