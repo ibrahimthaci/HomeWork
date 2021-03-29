9f8b786d1eeeb87572b4b588a0a26f618e9b31ca
ibrahimthaci <ibrahim_thaqi@outlook.com>
2021-03-29 02:08:37 +0200

    Update WordCounter.py

--- Week2/Second/WordCounter.py
+++ Week2/Second/WordCounter.py
@@ -1,17 +1,18 @@
 from collections import Counter
 from matplotlib import pyplot as plt
 
-file = open('TextFile.txt','r').read()
-words= file.lower().replace('?','').replace('.','').replace(',','').split()
-counter=Counter(words)
-topfive=counter.most_common(5)
-print(topfive)
+file = open('TextFile.txt','r').read() #readfile
+words= file.lower().replace('?','').replace('.','').replace(',','').split() #make all lowercase and replace , . ? etc to '' to make split
+counter=Counter(words) #create counter to count how many times they are used
+topfive=counter.most_common(5) # find 5 mos common words
+b=dict(topfive) #convert to dictionaries, to use keys and values on plot bar as keys and values
+print(b.values())
 
-keys=['it','at','or','he','to']
-values=[5,4,4,4,4]
+# keys=['it','at','or','he','to']
+# values=[5,4,4,4,4]
 
-plt.bar(keys,values)
-plt.title("Fjalët më të shpeshta")
-plt.ylabel("Numri i përseritjeve")
-plt.xlabel("Fjalët")
+plt.bar(b.keys(),b.values()) #p
+plt.title("TOP 5 MOST USED WORDS")
+plt.ylabel("WORDS FREQUENCY")
+plt.xlabel("WORDS")
 plt.show()
