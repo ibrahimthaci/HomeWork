0a58ec5cc73b598c92ed9c4cd537620558b89934
ibrahimthaci <ibrahim_thaqi@outlook.com>
2021-03-29 02:10:11 +0200

    Update WordCounter.py

--- Week2/Second/WordCounter.py
+++ Week2/Second/WordCounter.py
@@ -2,16 +2,16 @@ from collections import Counter
 from matplotlib import pyplot as plt
 
 file = open('TextFile.txt','r').read() #readfile
-words= file.lower().replace('?','').replace('.','').replace(',','').split() #make all lowercase and replace , . ? etc to '' to make split
+words= file.lower().replace('?','').replace('.','').replace(',','').split() #make all lowercase and replace , . ? etc to '' SO they can split
 counter=Counter(words) #create counter to count how many times they are used
 topfive=counter.most_common(5) # find 5 mos common words
-b=dict(topfive) #convert to dictionaries, to use keys and values on plot bar as keys and values
-print(b.values())
+b=dict(topfive) #convert to dictionaries, to use keys and values on plot bar 
+#print(b.values())
 
 # keys=['it','at','or','he','to']
 # values=[5,4,4,4,4]
 
-plt.bar(b.keys(),b.values()) #p
+plt.bar(b.keys(),b.values()) #ploting
 plt.title("TOP 5 MOST USED WORDS")
 plt.ylabel("WORDS FREQUENCY")
 plt.xlabel("WORDS")
