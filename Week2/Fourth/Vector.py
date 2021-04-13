b077c5869ae3fcf6b2ecdcec75ba72c7e8bcf331
ibrahimthaci <ibrahim_thaqi@outlook.com>
2021-04-13 16:06:33 +0200

    Update Vector.py

--- Week2/Fourth/Vector.py
+++ Week2/Fourth/Vector.py
@@ -1,8 +1,8 @@
 Vector1 = [1,2,3,4]
-Vector2 = [2,3,4,5]
+Vector2 = [3,3,4,5]
 
-def subtract(v:Vector1, w:Vector2):
-    assert len(v) == len(w), "Vectors must have same lenght"
-    return [v_i+w_i for v_i, w_i in zip(v,w)]
+def subtract(Vector1, Vector2):
+    assert len(Vector1) == len(Vector2), "Vectors must have same lenght"
+    return [v_i+w_i for v_i, w_i in zip(Vector1,Vector2)]
 
 print(subtract(Vector1,Vector2))
