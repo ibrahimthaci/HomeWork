fbb73d41943ee545cc23abdf160b060879f16960
ibrahimthaci <ibrahim_thaqi@outlook.com>
2021-04-13 16:10:31 +0200

    Update Vector.py

--- Week2/Fourth/Vector.py
+++ Week2/Fourth/Vector.py
@@ -1,7 +1,7 @@
 Vector1 = [1,2,3,4]
 Vector2 = [3,3,4,5]
 
-def subtract(Vector1, Vector2):
+def subtract(Vector1, Vector2):                     // vecotors have abr like (v:Vector1, w:Vector2)
     assert len(Vector1) == len(Vector2), "Vectors must have same lenght"
     return [v_i+w_i for v_i, w_i in zip(Vector1,Vector2)]
 
