ce5fee3acf8bdc481d67dc51eff6ea194b365b5a
ibrahimthaci <ibrahim_thaqi@outlook.com>
2021-04-13 16:09:39 +0200

    Update Vector.py

--- Week2/Fourth/Vector.py
+++ Week2/Fourth/Vector.py
@@ -6,3 +6,18 @@ def subtract(Vector1, Vector2):
     return [v_i+w_i for v_i, w_i in zip(Vector1,Vector2)]
 
 print(subtract(Vector1,Vector2))
+
+
+
+#or================
+
+Vector1 = [1,2,3,4]
+Vector2 = [52,3,4,5]
+
+def subtract(Vector1, Vector2):
+    assert len(Vector1) == len(Vector2), "Vectors must have same lenght"
+    return [v_i+w_i for v_i, w_i in zip(Vector1,Vector2)]
+
+if(len(Vector1)==len(Vector2)):
+    print(subtract(Vector1,Vector2))
+else:print("Length is not the same")
