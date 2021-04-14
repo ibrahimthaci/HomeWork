605aa9b0fcfbd3d571d2e8da1bf55ec8844abaff
ibrahimthaci <ibrahim_thaqi@outlook.com>
2021-04-14 05:40:20 +0200

    Update Vector.py

--- Week2/Fourth/Vector.py
+++ Week2/Fourth/Vector.py
@@ -1,23 +1,31 @@
-Vector1 = [1,2,3,4]
-Vector2 = [3,3,4,5]
-
-def subtract(Vector1, Vector2):                     // vecotors have abr like (v:Vector1, w:Vector2)
-    assert len(Vector1) == len(Vector2), "Vectors must have same lenght"
-    return [v_i+w_i for v_i, w_i in zip(Vector1,Vector2)]
-
-print(subtract(Vector1,Vector2))
-
-
-
-#or================
 
 Vector1 = [1,2,3,4]
 Vector2 = [52,3,4,5]
 
-def subtract(Vector1, Vector2):
+def addition(Vector1, Vector2):              // vecotors have abr like (v:Vector1, w:Vector2)
     assert len(Vector1) == len(Vector2), "Vectors must have same lenght"
+    return [v_i+w_i for v_i, w_i in zip(Vector1,Vector2)]
+    print(subtract(Vector1,Vector2))
+    
+============ OR ================    
+
+def (Vector1, Vector2):
     return [v_i+w_i for v_i, w_i in zip(Vector1,Vector2)]
 
 if(len(Vector1)==len(Vector2)):
     print(subtract(Vector1,Vector2))
 else:print("Length is not the same")
+
+ ============subtraction===============
+    
+def subtract(Vector1, Vector2):
+    assert len(Vector1) == len(Vector2), "Vectors must have same lenght"
+    return [v_i-w_i for v_i, w_i in zip(Vector1,Vector2)]
+
+if(len(Vector1)==len(Vector2)):
+    print(subtract(Vector1,Vector2))
+else:print("Length is not the same")
+
+
+
+
