00cf337f95fda5503ae2de9815f2d1deb1907c10
ibrahimthaci <ibrahim_thaqi@outlook.com>
2021-05-20 11:52:40 +0200

    Add files via upload

--- Week4/ParliamentMembers/KosovoDeputy2021.py
+++ Week4/ParliamentMembers/KosovoDeputy2021.py
@@ -5,6 +5,9 @@ url = "https://kuvendikosoves.org/shq/deputetet/"
 text = requests.get(url).text
 soup = BeautifulSoup(text,'lxml') #or instead of lxml == html.parser
 s = soup.find_all('h4', {'class':'name'})
-
 for i in s:
     print(i)
+
+#other way if we want to display without html tags
+for s in soup.find_all('h4', {'class':'name'}): 
+    print(str(s)[17:][0:-5])
