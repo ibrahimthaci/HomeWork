789d44fc3f57a4f2313cae24bad1c42f593c7b83
ibrahimthaci <ibrahim_thaqi@outlook.com>
2021-05-21 00:48:44 +0200

    Add files via upload

---
+++ Week4/Panda/CVS.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+df = pd.read_csv('Sample100.csv')
+
+print(df.to_string())
+
+
+
+#-----------------
+import pandas as pd
+mydataset = {
+  'cars':
+      ["BMW", "Volvo", "Ford"],
+  'passings':
+      [3, 7, 2]
+}
+
+df = pd.DataFrame(mydataset)
+print(df.to_string())
