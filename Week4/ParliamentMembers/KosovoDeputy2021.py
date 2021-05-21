5175a4c1704af43feaac052a98e2901115838b3c
ibrahimthaci <ibrahim_thaqi@outlook.com>
2021-05-21 23:38:32 +0200

    Add files via upload

---
+++ Week4/ParliamentMembers/KosovoDeputy2021.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://kuvendikosoves.org/shq/deputetet/"
+text = requests.get(url).text
+soup = BeautifulSoup(text,'lxml') #or instead of lxml == html.parser
+s = soup.find_all('h4', {'class':'name'})
+for i in s:
+    print(i) # if we add i.text would display only text inside h4 tag
+
+#other way if we want to display without html tags
+for s in soup.find_all('h4', {'class':'name'}):
+    print(str(s)[17:][0:-5]) #you can eaisly use [17:-5] in same [] to remove before and after
+
+
+    #or
+
+# url = "https://kuvendikosoves.org/shq/deputetet/"
+# html = requests.get(url).text
+# soup = BeautifulSoup(html, 'html5lib')
+# p = soup.find_all('h4', {'class': 'name'})
+# for i in p:
+#     print(i)
