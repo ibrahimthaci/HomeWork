a1f5635aa04eb17535d930c17f9c05262ffd3f3c
ibrahimthaci <ibrahim_thaqi@outlook.com>
2021-05-22 10:36:44 +0200

    Add files via upload

--- Week4/TwitterAPI/twitter_api.py
+++ Week4/TwitterAPI/twitter_api.py
@@ -1,1 +1,7 @@
-print("hi")
+import requests, json
+
+github_user = "joelgrus"
+endpoint = f"https://api.github.com/users/{github_user}/repos"
+repos = json.loads(requests.get(endpoint).text)
+for i in repos:
+    print(i)
