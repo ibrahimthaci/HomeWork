6c25f119e72afec74772460d3697164350a9c8a1
ibrahimthaci <ibrahim_thaqi@outlook.com>
2021-05-22 10:57:33 +0200

    Update twitter_api.py

--- Week4/TwitterAPI/twitter_api.py
+++ Week4/TwitterAPI/twitter_api.py
@@ -2,6 +2,6 @@ import requests, json
 
 github_user = "ibrahimthaci"
 endpoint = f"https://api.github.com/users/{github_user}/repos"
-repos = json.loads(requests.get(endpoint).text)
+repos = json.loads(requests.get(endpoint).text) #loads request in text form, json type
 for i in repos:
     print(i)
